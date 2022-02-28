e4dab30ec82b34c4198156dc95dfedf7c4b87f89
hammoudi Mohamed-Said <mohamedsaid.hammoudi.etu@univ-lille.fr>
2022-02-28 01:16:16 +0100

    feat: add swarm flight example withoute state

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from conf import *
 from djitellopy import TelloSwarm
 
-
 def swarm_example():
-    # create and initialize a configuration
-    conf = Configuration('router_name', 'router_ssid', 'router_password', 24)
-    conf.run()
-    if conf.get_tello_ip_swarm() is None:
-        logging.error("There is no available Tello drone")
-        return
-    swarm = TelloSwarm.fromIps(conf.get_tello_ip_swarm())
+    # create, initialize a configuration and save it in a variable for later
+    # change the parameter with your router's parameter
+    conf = Configuration('redme007', 'redme007', 'motdepasse', 24)
+    # run tello configuration script
+    ip = conf.run()
+
+    # create swarm  drone with available tello ip that we recover from conf variable
+    swarm = TelloSwarm.fromIps(ip)
     swarm.connect()
     swarm.takeoff()
-    swarm.move_forward(20)
+
+    swarm.move_forward(30)
+    swarm.move_down(30)
+    swarm.sequential(lambda i, tello: tello.move_up(i * 10 + 20))
+    swarm.sequential(lambda i, tello: tello.move_down(i * 10 + 20))
+    swarm.rotate_clockwise(180)
+    swarm.move_forward(30)
+    swarm.move_up(40)
+    swarm.move_down(40)
+
     swarm.land()
     swarm.end()
-
 
 if __name__ == '__main__':
     swarm_example()
